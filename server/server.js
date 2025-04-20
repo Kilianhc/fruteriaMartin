862c36822d8a7e204198532b87f64ec89814a691
Kilian H <kilianhc@hotmail.com>
2025-04-20 21:14:57 +0100

    create structure with server and client folders. Progress in server with auth&models.

---
+++ server/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import productRoutes from './routes/products.js';
+/* import userRoutes from './routes/users.js';
+import orderRoutes from './routes/orders.js'; */
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Rutas
+app.use('/api/products', productRoutes);
+/* app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes); */
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB conectado');
+    app.listen(5000, () => console.log('Servidor en puerto 5000'));
+  })
+  .catch(err => console.error(err));
