65b61c28ba74a883eab0a4eeedcf504627556c6c
Kilian H <kilianhc@hotmail.com>
2025-04-29 12:28:00 +0100

    upgrade in contact section

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,10 +1,9 @@
 function Footer() {
-    return (
-      <footer className="bg-[#bd0003] text-white text-center py-2 mt-15">
+  return (
+      <footer className="bg-[#bd0003] text-white text-center py-2">
         © 2025 Kilian D. Hernández Chirino. Todos los derechos reservados.
       </footer>
-    );
-  }
-  
-  export default Footer;
-  
+  );
+}
+
+export default Footer;
--- client/src/pages/Home/ContactSection.jsx
+++ client/src/pages/Home/ContactSection.jsx
@@ -2,36 +2,35 @@ import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt, FaWhatsapp, FaClock } from 'rea
 
 export default function ContactSection() {
   return (
-    <section className="bg-[#bd0003] text-white py-12 px-6 text-center">
-      <h2 className="text-3xl font-bold mb-8">Contáctanos</h2>
+    <section className="bg-gray-100" >
+      <div className="bg-[#bd0003] text-white py-12 px-6 text-center">
+        <h2 className="text-3xl font-bold mb-8">Contáctanos</h2>
 
-      <div className="space-y-6 max-w-xl mx-auto text-center">
-        <p className="flex justify-center items-start gap-3">
-          <FaMapMarkerAlt className="text-xl mt-1" />
-          C/ Galicia, 24, 35006, Mercado Central de Las Palmas de Gran Canaria
-        </p>
-        <p className="flex justify-center items-center gap-3">
-          <FaPhoneAlt className="text-xl" />
-          +34 612 345 678
-        </p>
-        <p className="flex justify-center items-center gap-3">
-          <FaEnvelope className="text-xl" />
-          contacto@fruteriamartin.com
-        </p>
-        <p className="flex justify-center items-center gap-3">
-          <FaClock className="text-xl" />
-          Lunes a Sábado: 8:00 - 14:00
-        </p>
-        <a
-          href="https://wa.me/34612345678"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="flex justify-center items-center gap-3 text-green-400 hover:underline"
-        >
-          <FaWhatsapp className="text-xl" />
-          Escríbenos por WhatsApp
-        </a>
+        <div className="space-y-6 max-w-xl mx-auto text-center">
+          <p className="flex justify-center items-start gap-3">
+            <FaMapMarkerAlt className="text-xl mt-1" />
+            C/ Galicia, 24, 35006, Mercado Central de Las Palmas de Gran Canaria
+          </p>
+          <p className="flex justify-center items-center gap-3">
+            <FaPhoneAlt className="text-xl" />
+            +34 612 345 678
+          </p>
+          <p className="flex justify-center items-center gap-3">
+            <FaEnvelope className="text-xl" />
+            contacto@fruteriamartin.com
+          </p>
+          <p className="flex justify-center items-center gap-3">
+            <FaClock className="text-xl" />
+            Lunes a Sábado: 8:00 - 14:00
+          </p>
+        </div>
       </div>
+      <a href="https://wa.me/34612345678" target="_blank" rel="noopener noreferrer"
+        className="flex justify-center pt-20 pb-20 items-center gap-3 text-[#009929] text-2xl font-bold hover:cursor"
+      >
+        <FaWhatsapp className="text-5xl font-bold" />
+        Escríbenos por WhatsApp
+      </a>
     </section>
   );
 }
